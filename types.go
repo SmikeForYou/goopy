b509b161c9a4812523ca81cdadc2e6ed84132554
Anatolii Savran <anatolii.savran@qcee.org>
2023-01-24 05:39:28 +0200

    Remove unnecessary types

--- types.go
+++ types.go
@@ -2,7 +2,6 @@ package goopy
 
 import (
 	"context"
-	"github.com/doug-martin/goqu/v9"
 	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/jackc/pgx/v5/pgconn"
 )
@@ -24,16 +23,4 @@ type rows interface {
 type exer interface {
 	pgxscan.Querier
 	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
-}
-
-type querier[T Model] interface {
-	Select(ctx context.Context, expressions ...goqu.Expression) ([]T, error)
-	SelectOne(ctx context.Context, expressions ...goqu.Expression) (T, error)
-}
-
-type manager[T Model] interface {
-	Insert(ctx context.Context, in T) (T, error)
-	BulkInsert(ctx context.Context, in ...T) ([]T, error)
-	Update(ctx context.Context, in T) (T, error)
-	Delete(ctx context.Context, in T) error
 }
